93330a73d4c89569c5cee4e067f8bce3c5e9eb7b
Taniya <taniya249854@gmail.com>
2020-07-20 16:54:25 +0530

    Modified forecast details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,4 @@
 const request=require('request')
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
 
 
 
@@ -28,7 +19,7 @@ const forecast = (latitude,longitude,callback) =>
         }
         else
         {
-            callback(undefined,response.body.current.weather_descriptions[0]+"...It is "+ response.body.current.temperature +" degrees out. There is a "+ response.body.current.precip+"% chance of rain and it feels like "+ response.body.current.feelslike+" degree out there.")
+            callback(undefined,response.body.current.weather_descriptions[0]+"...It is "+ response.body.current.temperature +" degrees out. There is a "+ response.body.current.precip+"% chance of rain and it feels like "+ response.body.current.feelslike+" degree out there and the humidity is:" + response.body.current.humidity +"%.")
         }
     }
     )
